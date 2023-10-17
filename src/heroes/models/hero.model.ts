83d6771f0329e5e73fafecfbd65b09a1a99bcad0
rxolve <rxolve@gmail.com>
2023-10-17 21:38:40 +0900

    feat: apply hero model auto commit

--- src/heroes/models/hero.model.ts
+++ src/heroes/models/hero.model.ts
@@ -5,6 +5,7 @@ import { HeroKilledDragonEvent } from '../events/impl/hero-killed-dragon.event';
 export class Hero extends AggregateRoot {
   constructor(private readonly id: string) {
     super();
+    this.autoCommit = true;
   }
 
   killEnemy(enemyId: string) {
