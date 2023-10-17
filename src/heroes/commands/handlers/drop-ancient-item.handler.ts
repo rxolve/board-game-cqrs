1dabc1f26ba2d8bd724f4c641fd1acdeead54c1c
rxolve <rxolve@gmail.com>
2023-10-17 21:39:52 +0900

    feat: apply mergeClassContext to all handlers

--- src/heroes/commands/handlers/drop-ancient-item.handler.ts
+++ src/heroes/commands/handlers/drop-ancient-item.handler.ts
@@ -2,24 +2,21 @@ import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import * as clc from 'cli-color';
 import { HeroRepository } from '../../repository/hero.repository';
 import { DropAncientItemCommand } from '../impl/drop-ancient-item.command';
+import { Hero } from 'src/heroes/models/hero.model';
 
 @CommandHandler(DropAncientItemCommand)
 export class DropAncientItemHandler
   implements ICommandHandler<DropAncientItemCommand>
 {
-  constructor(
-    private readonly repository: HeroRepository,
-    private readonly publisher: EventPublisher,
-  ) {}
+  constructor(private readonly publisher: EventPublisher) {}
 
   async execute(command: DropAncientItemCommand) {
     console.log(clc.yellowBright('Async DropAncientItemCommand...'));
 
     const { heroId, itemId } = command;
-    const hero = this.publisher.mergeObjectContext(
-      await this.repository.findOneById(+heroId),
-    );
+    const HeroModel = this.publisher.mergeClassContext(Hero);
+    const hero = new HeroModel(heroId);
+
     hero.addItem(itemId);
-    hero.commit();
   }
 }
--- src/heroes/commands/handlers/kill-dragon.handler.ts
+++ src/heroes/commands/handlers/kill-dragon.handler.ts
@@ -2,22 +2,20 @@ import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import * as clc from 'cli-color';
 import { HeroRepository } from '../../repository/hero.repository';
 import { KillDragonCommand } from '../impl/kill-dragon.command';
+import { Hero } from 'src/heroes/models/hero.model';
 
 @CommandHandler(KillDragonCommand)
 export class KillDragonHandler implements ICommandHandler<KillDragonCommand> {
-  constructor(
-    private readonly repository: HeroRepository,
-    private readonly publisher: EventPublisher,
-  ) {}
+  constructor(private readonly publisher: EventPublisher) {}
 
   async execute(command: KillDragonCommand) {
     console.log(clc.greenBright('KillDragonCommand...'));
 
     const { heroId, dragonId } = command;
-    const hero = this.publisher.mergeObjectContext(
-      await this.repository.findOneById(+heroId),
-    );
+    const HeroModel = this.publisher.mergeClassContext(Hero);
+    const hero = new HeroModel(heroId);
     hero.killEnemy(dragonId);
-    hero.commit();
+
+    return 'OK';
   }
 }
